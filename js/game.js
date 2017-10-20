a3900138a5de8099e5e2bb8429ee13dbc54b76b5
Schu1609 <schu1609@student.nhl.nl>
2017-10-20 16:05:57 +0200

    game.js bijna af moet alleen animate nog aanroepen

--- js/game.js
+++ js/game.js
@@ -4,24 +4,47 @@
 class Game {
     constructor(){
         this.controllers = {};
-        this.players;
+        this.playercolors = {};
+        this.players = {};
+        this.totalplayers;
         this.turns;
-        this.world = "Hello World, type here the number of players: <input required type=\"number\" min=\"2\" max=\"8\" value=\"2\" id=\"ptext\" > " +
+        this.colors = ["red", "blue", "yellow", "green", "pink", "black", "white"];
+        this.hexcolors = [ 0xf45c42, 0x0033cc, 0xffff00, 0x00ff00, 0xffb3e6, 0x000000, 0xffffff];
+        this.world = "Hello World, type here the number of totalplayers: <input required type=\"number\" min=\"2\" max=\"8\" value=\"2\" id=\"ptext\" > " +
             "<input type='button' value='Everdien' onclick='getText()'>";
         document.getElementById("game").innerHTML = this.world;
     }
     startgame()
     {
-
+        for (var i = 0; i < this.totalplayers; i++)
+        {
+            this.players[i] = new Player(i+1, new Snake(this.playercolors[i]), this.controllers[i]);
+        }
+        players = this.players;
     }
     setcontrols()
     {
         var playercontrols;
-        for (var i = 0; i < this.players; i++)
+        for (var i = 0; i < this.totalplayers; i++)
         {
             playercontrols = document.getElementsByName("player" + (i+1));
-            var tempcontrols = new Controls(playercontrols[0].value,playercontrols[1].value,playercontrols[2].value );
-            this.controllers[i] = tempcontrols;
+            //var tempcontrols = new Controls(playercontrols[0].value,playercontrols[1].value,playercontrols[2].value );
+            this.controllers[i] = new Controls(playercontrols[0].value,playercontrols[1].value,playercontrols[2].value );
+        }
+    }
+    setcolors()
+    {
+        var scolor = document.getElementById("player1").value;
+        for (var i = 0; i < this.totalplayers; i++)
+        {
+            for (var j = 0; j < this.colors.length; j++)
+            {
+                if (document.getElementById("player" + (i+1)).value == this.colors[j])
+                {
+                    this.playercolors[i] = this.hexcolors[j];
+                    break;
+                }
+            }
         }
     }
     gettext()
@@ -31,13 +54,16 @@ class Game {
             ptext = 8;
         if (ptext < 2)
             ptext = 2;
-        this.players = ptext;
+        this.totalplayers = ptext;
         var playerfield = "";
         for (var i = 0; i < ptext; i++)
         {
             playerfield += "<p>Player " + (i+1)+ " <input type='text' value='left' name= 'player" + (i+1) + "' > " +
                 "<input type='text' value='right' name= 'player" + (i + 1) + "' >" +
-                "<input type='text' value='jump' name='player" + (i + 1) + "' > <br>"
+                "<input type='text' value='jump' name='player" + (i + 1) + "' > <select id='player" + (i + 1) + "'> ";
+            for (var j = 0; j < this.colors.length; j++)
+                playerfield += "<option value='" + this.colors[j] + "'>" + this.colors[j] + "</option>"
+            playerfield += "</select> <br>";
         }
         document.getElementById("players").innerHTML = playerfield + "<input type='button' value='okay' onclick='setControls()'> <br> ";
     }
@@ -51,4 +77,5 @@ function getText(){
 function setControls()
 {
     game.setcontrols();
+    game.setcolors();
 }
