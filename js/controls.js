605cad6b53a00cc59da2494961adb37d28ee867c
Schu1609 <schu1609@student.nhl.nl>
2017-11-05 14:13:11 +0100

    Controls aangepast

--- js/controls.js
+++ js/controls.js
@@ -5,19 +5,33 @@
 class Controls
 {
     constructor (left,right,jump){
-        this.left = left.toUpperCase().charCodeAt(0);
-        this.right = right.toUpperCase().charCodeAt(0);
-        this.jump = jump.toUpperCase().charCodeAt(0);
-        if(left === "LEFT" || left === "left")
-            this.left = 37;
-        if (right === "RIGHT" || right === "right")
-            this.right = 39;
-        if (jump === "JUMP" || jump === "jump")
-            this.jump = 32;
-        if (jump === "UP" || jump === "up")
-            this.jump = 38;
-        if (jump === "DOWN" || jump === "down")
-            this.jump = 40;
+        var defaultList = {'LEFT' : 37, 'RIGHT' : 39, 'JUMP' : 32, 'DOWN' : 40};
+        // var leftDefault = defaultList[left.toLowerCase()];
+        // var rightDefault = defaultList[right.toLowerCase()];
+        // var jumpDefault  = defaultList[jump.toLowerCase()];
+
+        left = left?  left.toUpperCase(): 'LEFT';
+        right = right? right.toUpperCase(): 'RIGHT';
+        jump = jump? jump.toUpperCase(): 'JUMP';
+
+        this.left = left in defaultList? defaultList[left] : left.charCodeAt(0);
+        this.right = right in defaultList? defaultList[right] : right.charCodeAt(0);
+        this.jump = jump in defaultList? defaultList[jump] : jump.charCodeAt(0);
+
+
+        //
+        // if(left === "LEFT" || left === "left")
+        //     this.left = 37;
+        // if (right === "RIGHT" || right === "right")
+        //     this.right = 39;
+        // if (jump === "JUMP" || jump === "jump")
+        //     this.jump = 32;
+        // if (jump === "UP" || jump === "up")
+        //     this.jump = 38;
+        // if (jump === "DOWN" || jump === "down")
+        //     this.jump = 40;
+
+
         this.keyboard = {};
         this.keyboard[this.left] = false;
         this.keyboard[this.right] = false;
